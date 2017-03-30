c0abecc6792f277debf3dc452e61701f4e589be9
Abdullah Shekhar <Abdullah Shekhar@192.168.0.6>
2017-03-30 16:49:31 +0600

    Some algorithm

---
+++ src/hackerrankpractice/java/introduction/Loops.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2017 Abdullah Shekhar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package hackerrankpractice.java.introduction;
+
+/*
+Objective 
+In this challenge, we're going to use loops to help us do some simple math.
+
+Task 
+Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: N x i = result.
+
+Input Format
+
+A single integer, .
+
+Constraints
+
+Output Format
+
+Print  lines of output; each line  (where ) contains the  of  in the form: 
+N x i = result.
+
+Sample Input
+
+2
+Sample Output
+
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+*/
+import java.util.Scanner;
+/**
+ *
+ * @author Abdullah Shekhar
+ */
+public class Loops {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        
+        for(int i=1;i<=10;i++){
+            System.out.println(N + " x " + i+" = "+(N*i));
+        }
+    }
+}
